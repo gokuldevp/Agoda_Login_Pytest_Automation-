d87706a80763019301cd6594a2f7e5361b3cef2f
gokuldev <gokuldev.p123@gmail.com>
2025-01-24 22:03:14 +0530

    update

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
-pip install pytest
-pip install selenium
-pip install webdriver-manager
-pip install pytest-xdist
-pip install pytest-html
-pip install pytest-dependency
-pip install pytest-selenium
-pip install pytest-ordering
-pip install faker
+@echo off
+REM Activate the virtual environment
+call .\.venv\Scripts\activate
+
+REM Run pytest
+pytest -s -v ./test_cases/test_login.py
+
+REM Deactivate the virtual environment
+deactivate
